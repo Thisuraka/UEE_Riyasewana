b5c958bb9fce24115efa6d93f0b763136b89a41d
vibudathisuraka <vibudathisuraka@gmail.com>
2021-09-24 02:00:08 +0530

    view vehicle

---
+++ lib/widgets/custom_slider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../styles.dart';
+
+class CustomSlider extends StatefulWidget {
+  CustomSlider({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _CustomSliderState createState() => _CustomSliderState();
+}
+
+class _CustomSliderState extends State<CustomSlider> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
